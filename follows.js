cdf8629316212a5a86c7ccfda8dd7e564a40529a
faizirizvi <saghar.abbas@gmail.com>
2015-03-17 20:09:25 +0530

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'dTH6WVh7yEj3Oe4dyHSJ7owxgVSYtEVmkLuPpoAIgwEfxwzf', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1161827086], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+		applicationName	= 'Saghar App', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
